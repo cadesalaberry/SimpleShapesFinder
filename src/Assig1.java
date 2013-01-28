28e9bd3490ed0b72595f0ecb92f8044dc11f7541
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-01-28 11:04:55 -0500

    Mod: Working algorithm. Does not filter out duplicates.

--- src/Assig1.java
+++ src/Assig1.java
@@ -27,13 +27,24 @@ public class Assig1 {
 			// if present, arg 4 is the max height we consider
 			int h = (args.length > 4) ? Integer.parseInt(args[4]) : img
 					.getHeight();
+			
+			// Try all radii between minr and maxr
+			for (int radius = minr; radius < maxr; radius++) {
+				
+				// Try all x coordinate potentially containing a circle of specified radius
+				for (int width = radius + 1; width < w - radius - 1; width++) {
+					
+					// Try all y coordinate potentially containing a circle of specified radius
+					for (int height = radius + 1; height < h - radius - 1; height++) {
 
-
-			for (int width = 0; width < w; width++) {
-				for (int height = 0; height < h; height++) {
-					for (int radius = minr; radius < maxr; radius++) {
+						
+						
 						if (isCircle(width, height, radius, img)) {
+
+							System.out.println("Circle Found: [" + width + "," + height
+									+ "] @r=" + radius);
 							drawCircle(width, height, radius, img, g2);
+
 						}
 					}
 				}
@@ -96,25 +107,54 @@ public class Assig1 {
 			g.drawLine(cx - y, cy - x, cx - y, cy - x);
 		}
 	}
-	
+
 	static boolean isCircle(int cx, int cy, int r, BufferedImage img) {
+
+		int externalColor = getColorOfCircle(cx, cy, r + 1, img);
+		int internalColor = getColorOfCircle(cx, cy, r - 1, img);
+		int circleColor = getColorOfCircle(cx, cy, r, img);
+		
+		
+		// Checks if a circle exist with the specified radius.
+		if (isACircle(cx, cy, r, img)) {
+		
+			// Checks that the inner circle if of different color.
+			if (isACircle(cx, cy, r - 1, img) && internalColor != circleColor) {
+				
+				//System.out.println("Circles inside and on r=" + r + "@" + cx + ";" + cy);
+				
+				// Checks that the outter circle is of different color.
+				if (isACircle(cx, cy, r + 1, img) && externalColor != circleColor) {
+					//System.out.println("Circle Found: " + "[" + cx + "," + cy + "] @r=" + r);
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	static int getColorOfCircle(int cx, int cy, int r, BufferedImage img) {
+		return img.getRGB(cx, cy + r);
+	}
+	static boolean isACircle(int cx, int cy, int r, BufferedImage img) {
+
 		int f = 1 - r;
 		int ddF_x = 1;
 		int ddF_y = -2 * r;
 		int x = 0;
 		int y = r;
-		
-		// you can look at pixel values with this API call:
-		 // get RGB value of pixel at (0,0)
+		boolean isCircle = true;
 
-		// Checks cardinal points
-		int c = img.getRGB(cx, cy + r);
-		int south = img.getRGB(cx, cy - r);
-		int east = img.getRGB(cx + r, cy);
-		int west = img.getRGB(cx - r, cy);
+		// Checks if color at cardinal points match
+		final int c = img.getRGB(cx, cy + r);
+
+		isCircle = isCircle && (c == img.getRGB(cx, cy - r));
+		isCircle = isCircle && (c == img.getRGB(cx + r, cy));
+		isCircle = isCircle && (c == img.getRGB(cx - r, cy));
 
 		// draw 1/8 of the circle, taking advantage of symmetry
-		while (x < y) {
+		while (x < y && isCircle) {
 			if (f >= 0) {
 				y--;
 				ddF_y += 2;
@@ -124,14 +164,16 @@ public class Assig1 {
 			ddF_x += 2;
 			f += ddF_x;
 
-			g.drawLine(cx + x, cy + y, cx + x, cy + y);
-			g.drawLine(cx - x, cy + y, cx - x, cy + y);
-			g.drawLine(cx + x, cy - y, cx + x, cy - y);
-			g.drawLine(cx - x, cy - y, cx - x, cy - y);
-			g.drawLine(cx + y, cy + x, cx + y, cy + x);
-			g.drawLine(cx - y, cy + x, cx - y, cy + x);
-			g.drawLine(cx + y, cy - x, cx + y, cy - x);
-			g.drawLine(cx - y, cy - x, cx - y, cy - x);
+			isCircle = isCircle && c == img.getRGB(cx + x, cy + y);
+			isCircle = isCircle && c == img.getRGB(cx - x, cy + y);
+			isCircle = isCircle && c == img.getRGB(cx + x, cy - y);
+			isCircle = isCircle && c == img.getRGB(cx - x, cy - y);
+			isCircle = isCircle && c == img.getRGB(cx + y, cy + x);
+			isCircle = isCircle && c == img.getRGB(cx - y, cy + x);
+			isCircle = isCircle && c == img.getRGB(cx + y, cy - x);
+			isCircle = isCircle && c == img.getRGB(cx - y, cy - x);
 		}
+		//System.out.println(isCircle);
+		return isCircle;
 	}
 }
